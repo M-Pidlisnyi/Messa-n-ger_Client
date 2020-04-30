968cfef10655563113d4f6a65c1f4dc716855439
misha_pidlinsyi <mishapropatriot@gmail.com>
2020-04-30 14:24:08 +0300

    divided RunClient into 3 methods several bugs detected

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Client
 {
@@ -22,36 +10,27 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IPHostEntry ip_host;
+        private IPAddress ip_address;
+        private IPEndPoint ip_endpoint;
+
+        private Socket sender_socket = new Socket(AddressFamily.InterNetwork,
+                                       SocketType.Stream,
+                                       ProtocolType.Tcp);
+
+        private byte[] recieved_bytes = new byte[2048];
+
         public MainWindow()
         {
             InitializeComponent();
-           
+
+            Connect();    
         }
 
-
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Connect()
         {
-            RunServer();
-        }
-
-        public  void Write(string input)
-        {
-            Output.Text = input;
-        }
-
-        public  string ReadLine()
-        {
-            return Input.Text;
-        }
-       
-        private void RunServer()
-        {
-            byte[] recieved_bytes = new byte[2048];
-
-            var ip_host = Dns.GetHostEntry(new IPAddress(new byte[] { 192, 168, 1, 103 }));
-            IPAddress ip_address = default;
+            ip_host = Dns.GetHostEntry(new IPAddress(new byte[] { 192, 168, 1, 103 }));
+            ip_address = default;
 
             foreach (var current_ip in ip_host.AddressList)
             {
@@ -61,27 +40,53 @@ namespace Client
                 }
             }
 
-            var ip_endpoint = new IPEndPoint(ip_address, 2000);
+            ip_endpoint = new IPEndPoint(ip_address, 2000);
 
-            Write("Starting, creating socket\n");
-            Socket sender = new Socket(AddressFamily.InterNetwork,
-                                       SocketType.Stream,
-                                       ProtocolType.Tcp);
-
-            sender.Connect(ip_endpoint);
-            Write($"Succsesfully connected to {sender.RemoteEndPoint}\n");
-
-            Write("Enter message to server");
-            string message_to_server = ReadLine();
-
-            Write("Creating message");
-            byte[] sending_bytes = Encoding.UTF8.GetBytes(message_to_server);
-            sender.Send(sending_bytes);
-            int total_recieved_bytes = sender.Receive(recieved_bytes);
-            Write($"Message provided from server: {Encoding.UTF8.GetString(recieved_bytes)}");
-            sender.Close();
+            sender_socket.Connect(ip_endpoint);
+            WriteDebug($"Succesfully connected to {sender_socket.RemoteEndPoint}\n");
         }
 
+        private void SendMessage(object sender, RoutedEventArgs e)
+        {
+            WriteDebug("Enter message to server");
+            string message_to_server = ReadLine();
+
+            WriteDebug("Creating message");
+            byte[] sending_bytes = Encoding.UTF8.GetBytes(message_to_server);
+            sender_socket.Send(sending_bytes);
+
+            
+        }
+
+        private void RefreshMessages(object sender, RoutedEventArgs e)
+        {
+            int total_recieved_bytes = sender_socket.Receive(recieved_bytes);
+            WriteDebug($"Message provided from server: {Encoding.UTF8.GetString(recieved_bytes)}");
+            Write(Encoding.UTF8.GetString(recieved_bytes));
+        }
+
+        private void CloseClient(object sender, RoutedEventArgs e)
+        {
+            sender_socket.Close();
+            Close();
+        }
+
+        private void Write(string input)
+        {
+            Output.Text += input;
+        }
+
+        private void WriteDebug(string input)
+        {
+            DebugInfo.Text += input;
+        }
+
+        private string ReadLine()
+        {
+            return Input.Text;
+        }
+
+        
     }  
 
     
